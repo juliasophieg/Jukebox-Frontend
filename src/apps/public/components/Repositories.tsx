89e609c04a29ffd87376688ef79f58f736a8027c
Julia <jsgoransson@gmail.com>
2024-10-10 10:42:03 +0200

    styling for repositories section

--- src/apps/public/components/Repositories.tsx
+++ src/apps/public/components/Repositories.tsx
@@ -11,16 +11,19 @@ export const Repositories = () => {
 
   const sections = [
     {
+      number: 1,
       title: 'Some Title',
       content:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris',
     },
     {
+      number: 2,
       title: 'Another Title',
       content:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris',
     },
     {
+      number: 3,
       title: 'Example Title',
       content:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris',
@@ -39,14 +42,21 @@ export const Repositories = () => {
         <div className="accordion">
           {sections.map((section, index) => (
             <div key={index}>
-              <button
+              <div
                 onClick={() => toggleSection(index)}
                 className="accordion-header"
               >
-                {section.title}
-              </button>
+                <div className="accordion-title">
+                  <div className="repository-number">0{section.number}</div>
+                  <h3>{section.title}</h3>
+                </div>
+                <button>{activeIndex === index ? '-' : '+'}</button>
+              </div>
               {activeIndex === index && (
-                <div className="accordion-content">{section.content}</div>
+                <div className="accordion-content">
+                  <p>{section.content}</p>
+                  <div className="content-image"></div>
+                </div>
               )}
             </div>
           ))}
